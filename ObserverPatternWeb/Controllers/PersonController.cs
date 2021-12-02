a701aad110d8aa9af6f965473dc759c463006221
WenXine <aakkssqq@gmail.com>
2021-12-02 22:03:18 +0800

    套板完成

---
+++ ObserverPatternWeb/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObserverPatternWeb.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
